75c5d0fddf8866732ea3eeafe6516376e118dde9
bennyk0406 <kbenny0406@gmail.com>
2022-08-30 23:10:23 +0900

    2022chuseok

--- src/prob/data/2022/6_chuseok.ts
+++ src/prob/data/2022/6_chuseok.ts
@@ -2,6 +2,253 @@ const itemData = {
     high: [
         {
             name: "우주 배달부 사기꾼",
+            probability: 1.924,
+            isEquipItem: true
+        },
+        {
+            name: "우주 구급대원 간호사",
+            probability: 1.924,
+            isEquipItem: true
+        },
+        {
+            name: "토끼봇 테두리",
+            probability: 1.924,
+            isEquipItem: true
+        },
+        {
+            name: "천체의 유영 테두리",
+            probability: 1.924,
+            isEquipItem: true
+        },
+        {
+            name: "소형 구급 우주선",
+            probability: 1.924,
+            isEquipItem: true
+        },
+        {
+            name: "빛나는 유성우",
+            probability: 1.924,
+            isEquipItem: true
+        },
+        {
+            name: "구급 토끼봇 지갑",
+            probability: 1.406,
+            isEquipItem: true
+        },
+        {
+            name: "우주 달토끼 짐승인간",
+            probability: 0.925,
+            isEquipItem: true
+        },
+        {
+            name: "우주 감시자 용병",
+            probability: 0.925,
+            isEquipItem: true
+        },
+        {
+            name: "용맹의 증표 테두리",
+            probability: 0.925,
+            isEquipItem: true
+        },
+        {
+            name: "우주복 헬멧 테두리",
+            probability: 0.925,
+            isEquipItem: true
+        },
+        {
+            name: "호랑이의 꼬리",
+            probability: 0.925,
+            isEquipItem: true
+        },
+        {
+            name: "달토끼의 우주패드",
+            probability: 0.925,
+            isEquipItem: true
+        },
+        {
+            name: "달토끼의 우주 지갑",
+            probability: 0.555,
+            isEquipItem: true
+        },
+        {
+            name: "홍단 스파이",
+            probability: 0.925,
+            isEquipItem: true
+        },
+        {
+            name: "청단 영매",
+            probability: 0.925,
+            isEquipItem: true
+        },
+        {
+            name: "보름달이 들어준 소원",
+            probability: 0.925,
+            isEquipItem: true
+        },
+        {
+            name: "한가위 기념 테두리",
+            probability: 0.925,
+            isEquipItem: true
+        },
+        {
+            name: "화투 42",
+            probability: 0.925,
+            isEquipItem: true
+        },
+        {
+            name: "신비한 합창",
+            probability: 0.555,
+            isEquipItem: true
+        },
+        {
+            name: "토끼 지갑",
+            probability: 0.555,
+            isEquipItem: true
+        },
+        {
+            name: "돼지 지갑",
+            probability: 0.555,
+            isEquipItem: true
+        },
+        {
+            name: "만월의 짐승인간",
+            probability: 0.925,
+            isEquipItem: true
+        },
+        {
+            name: "달토끼 영매",
+            probability: 0.925,
+            isEquipItem: true
+        },
+        {
+            name: "방아꾼 건달",
+            probability: 0.925,
+            isEquipItem: true
+        },
+        {
+            name: "토끼 테두리",
+            probability: 0.925,
+            isEquipItem: true
+        },
+        {
+            name: "청룡 : 정의의 파도 경찰",
+            probability: 0.925,
+            isEquipItem: true
+        },
+        {
+            name: "백호 : 치유의 바람 의사",
+            probability: 0.925,
+            isEquipItem: true
+        },
+        {
+            name: "구미호 마담",
+            probability: 0.925,
+            isEquipItem: true
+        },
+        {
+            name: "도깨비 기자",
+            probability: 0.925,
+            isEquipItem: true
+        },
+        {
+            name: "귀면문 테두리",
+            probability: 0.925,
+            isEquipItem: true
+        },
+        {
+            name: "여우령 테두리",
+            probability: 0.925,
+            isEquipItem: true
+        },
+        {
+            name: "아가씨의 아얌",
+            probability: 0.925,
+            isEquipItem: true
+        },
+        {
+            name: "도깨비의 허리장식",
+            probability: 0.925,
+            isEquipItem: true
+        },
+        {
+            name: "자색 비단 지갑",
+            probability: 0.555,
+            isEquipItem: true
+        },
+        {
+            name: "청룡의 뿔",
+            probability: 3.15,
+            isEquipItem: false
+        },
+        {
+            name: "백호의 이빨",
+            probability: 3.15,
+            isEquipItem: false
+        },
+        {
+            name: "달토끼 운석",
+            probability: 3.15,
+            isEquipItem: false
+        },
+        {
+            name: "한가위 기념 엽서",
+            probability: 8.82,
+            isEquipItem: false
+        },
+        {
+            name: "보름달 엽서",
+            probability: 8.82,
+            isEquipItem: false
+        },
+        {
+            name: "한가위 기념 확성기",
+            probability: 8.82,
+            isEquipItem: false
+        },
+        {
+            name: "호랑이 꼬리",
+            probability: 4.095,
+            isEquipItem: false
+        },
+        {
+            name: "토끼 꼬리",
+            probability: 4.095,
+            isEquipItem: false
+        },
+        {
+            name: "낡은 징벌의 엽서",
+            probability: 3.15,
+            isEquipItem: false
+        },
+        {
+            name: "징벌의 엽서",
+            probability: 3.15,
+            isEquipItem: false
+        },
+        {
+            name: "주사위 엽서",
+            probability: 3.15,
+            isEquipItem: false
+        },
+        {
+            name: "깜짝 엽서",
+            probability: 3.15,
+            isEquipItem: false
+        },
+        {
+            name: "4티어 카드",
+            probability: 3.15,
+            isEquipItem: false
+        },
+        {
+            name: "3티어 카드",
+            probability: 3.15,
+            isEquipItem: false
+        }
+    ],
+    low: [
+        {
+            name: "우주 배달부 사기꾼",
             probability: 0.39,
             isEquipItem: true
         },
@@ -298,253 +545,6 @@ const itemData = {
         {
             name: "2티어 카드",
             probability: 1.85,
-            isEquipItem: false
-        }
-    ],
-    low: [
-        {
-            name: "우주 배달부 사기꾼",
-            probability: 1.924,
-            isEquipItem: false
-        },
-        {
-            name: "우주 구급대원 간호사",
-            probability: 1.924,
-            isEquipItem: false
-        },
-        {
-            name: "토끼봇 테두리",
-            probability: 1.924,
-            isEquipItem: false
-        },
-        {
-            name: "천체의 유영 테두리",
-            probability: 1.924,
-            isEquipItem: false
-        },
-        {
-            name: "소형 구급 우주선",
-            probability: 1.924,
-            isEquipItem: false
-        },
-        {
-            name: "빛나는 유성우",
-            probability: 1.924,
-            isEquipItem: false
-        },
-        {
-            name: "구급 토끼봇 지갑",
-            probability: 1.406,
-            isEquipItem: false
-        },
-        {
-            name: "우주 달토끼 짐승인간",
-            probability: 0.925,
-            isEquipItem: true
-        },
-        {
-            name: "우주 감시자 용병",
-            probability: 0.925,
-            isEquipItem: true
-        },
-        {
-            name: "용맹의 증표 테두리",
-            probability: 0.925,
-            isEquipItem: true
-        },
-        {
-            name: "우주복 헬멧 테두리",
-            probability: 0.925,
-            isEquipItem: true
-        },
-        {
-            name: "호랑이의 꼬리",
-            probability: 0.925,
-            isEquipItem: true
-        },
-        {
-            name: "달토끼의 우주패드",
-            probability: 0.925,
-            isEquipItem: true
-        },
-        {
-            name: "달토끼의 우주 지갑",
-            probability: 0.555,
-            isEquipItem: true
-        },
-        {
-            name: "홍단 스파이",
-            probability: 0.925,
-            isEquipItem: true
-        },
-        {
-            name: "청단 영매",
-            probability: 0.925,
-            isEquipItem: true
-        },
-        {
-            name: "보름달이 들어준 소원",
-            probability: 0.925,
-            isEquipItem: true
-        },
-        {
-            name: "한가위 기념 테두리",
-            probability: 0.925,
-            isEquipItem: true
-        },
-        {
-            name: "화투 42",
-            probability: 0.925,
-            isEquipItem: true
-        },
-        {
-            name: "신비한 합창",
-            probability: 0.555,
-            isEquipItem: true
-        },
-        {
-            name: "토끼 지갑",
-            probability: 0.555,
-            isEquipItem: true
-        },
-        {
-            name: "돼지 지갑",
-            probability: 0.555,
-            isEquipItem: true
-        },
-        {
-            name: "만월의 짐승인간",
-            probability: 0.925,
-            isEquipItem: true
-        },
-        {
-            name: "달토끼 영매",
-            probability: 0.925,
-            isEquipItem: true
-        },
-        {
-            name: "방아꾼 건달",
-            probability: 0.925,
-            isEquipItem: true
-        },
-        {
-            name: "토끼 테두리",
-            probability: 0.925,
-            isEquipItem: true
-        },
-        {
-            name: "청룡 : 정의의 파도 경찰",
-            probability: 0.925,
-            isEquipItem: true
-        },
-        {
-            name: "백호 : 치유의 바람 의사",
-            probability: 0.925,
-            isEquipItem: true
-        },
-        {
-            name: "구미호 마담",
-            probability: 0.925,
-            isEquipItem: true
-        },
-        {
-            name: "도깨비 기자",
-            probability: 0.925,
-            isEquipItem: true
-        },
-        {
-            name: "귀면문 테두리",
-            probability: 0.925,
-            isEquipItem: true
-        },
-        {
-            name: "여우령 테두리",
-            probability: 0.925,
-            isEquipItem: true
-        },
-        {
-            name: "아가씨의 아얌",
-            probability: 0.925,
-            isEquipItem: true
-        },
-        {
-            name: "도깨비의 허리장식",
-            probability: 0.925,
-            isEquipItem: true
-        },
-        {
-            name: "자색 비단 지갑",
-            probability: 0.555,
-            isEquipItem: true
-        },
-        {
-            name: "청룡의 뿔",
-            probability: 3.15,
-            isEquipItem: false
-        },
-        {
-            name: "백호의 이빨",
-            probability: 3.15,
-            isEquipItem: false
-        },
-        {
-            name: "달토끼 운석",
-            probability: 3.15,
-            isEquipItem: false
-        },
-        {
-            name: "한가위 기념 엽서",
-            probability: 8.82,
-            isEquipItem: false
-        },
-        {
-            name: "보름달 엽서",
-            probability: 8.82,
-            isEquipItem: false
-        },
-        {
-            name: "한가위 기념 확성기",
-            probability: 8.82,
-            isEquipItem: false
-        },
-        {
-            name: "호랑이 꼬리",
-            probability: 4.095,
-            isEquipItem: false
-        },
-        {
-            name: "토끼 꼬리",
-            probability: 4.095,
-            isEquipItem: false
-        },
-        {
-            name: "낡은 징벌의 엽서",
-            probability: 3.15,
-            isEquipItem: false
-        },
-        {
-            name: "징벌의 엽서",
-            probability: 3.15,
-            isEquipItem: false
-        },
-        {
-            name: "주사위 엽서",
-            probability: 3.15,
-            isEquipItem: false
-        },
-        {
-            name: "깜짝 엽서",
-            probability: 3.15,
-            isEquipItem: false
-        },
-        {
-            name: "4티어 카드",
-            probability: 3.15,
-            isEquipItem: false
-        },
-        {
-            name: "3티어 카드",
-            probability: 3.15,
             isEquipItem: false
         }
     ]
